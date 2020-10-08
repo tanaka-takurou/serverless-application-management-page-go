696567bea9f0229319dfdb99fbac2a3acbde687a
tanaka.takurou <tanaka.takurou@icloud.com>
2020-10-08 12:11:28 +0900

    fix for aws-sdk-go-v2 v0.26.0

--- api/main.go
+++ api/main.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"bytes"
 	"context"
 	"strings"
+	"reflect"
 	"net/http"
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
 	"github.com/aws/aws-sdk-go-v2/service/serverlessapplicationrepository"
 )
 
@@ -93,18 +96,17 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 
 func getApplications(ctx context.Context)([]Application, error) {
 	if serverlessApplicationRepositoryClient == nil {
-		serverlessApplicationRepositoryClient = serverlessapplicationrepository.New(cfg)
+		serverlessApplicationRepositoryClient = getSARClient()
 	}
-	req := serverlessApplicationRepositoryClient.ListApplicationsRequest(&serverlessapplicationrepository.ListApplicationsInput{})
-	res, err := req.Send(ctx)
+	res, err := serverlessApplicationRepositoryClient.ListApplications(ctx, &serverlessapplicationrepository.ListApplicationsInput{})
 	if err != nil {
 		return nil, err
 	}
 	var applicationList []Application
-	for _, i := range res.ListApplicationsOutput.Applications {
+	for _, i := range res.Applications {
 		applicationList = append(applicationList, Application{
-			Name:        aws.StringValue(i.Name),
-			Description: aws.StringValue(i.Description),
+			Name:        stringValue(i.Name),
+			Description: stringValue(i.Description),
 			Stack:       Stack{},
 		})
 	}
@@ -117,17 +119,16 @@ func getApplications(ctx context.Context)([]Application, error) {
 
 func getApplicationId(ctx context.Context, name string)(string, error) {
 	if serverlessApplicationRepositoryClient == nil {
-		serverlessApplicationRepositoryClient = serverlessapplicationrepository.New(cfg)
+		serverlessApplicationRepositoryClient = getSARClient()
 	}
-	req := serverlessApplicationRepositoryClient.ListApplicationsRequest(&serverlessapplicationrepository.ListApplicationsInput{})
-	res, err := req.Send(ctx)
+	res, err := serverlessApplicationRepositoryClient.ListApplications(ctx, &serverlessapplicationrepository.ListApplicationsInput{})
 	if err != nil {
 		return "", err
 	}
 	var applicationId string
-	for _, i := range res.ListApplicationsOutput.Applications {
-		if name == aws.StringValue(i.Name) {
-			applicationId = aws.StringValue(i.ApplicationId)
+	for _, i := range res.Applications {
+		if name == stringValue(i.Name) {
+			applicationId = stringValue(i.ApplicationId)
 			break
 		}
 	}
@@ -136,48 +137,45 @@ func getApplicationId(ctx context.Context, name string)(string, error) {
 
 func getTemplateUrl(ctx context.Context, applicationId string)(string, error) {
 	if serverlessApplicationRepositoryClient == nil {
-		serverlessApplicationRepositoryClient = serverlessapplicationrepository.New(cfg)
+		serverlessApplicationRepositoryClient = getSARClient()
 	}
-	req := serverlessApplicationRepositoryClient.CreateCloudFormationTemplateRequest(&serverlessapplicationrepository.CreateCloudFormationTemplateInput{
+	res, err := serverlessApplicationRepositoryClient.CreateCloudFormationTemplate(ctx, &serverlessapplicationrepository.CreateCloudFormationTemplateInput{
 		ApplicationId: aws.String(applicationId),
 	})
-	res, err := req.Send(ctx)
 	if err != nil {
 		return "", err
 	}
-	return aws.StringValue(res.CreateCloudFormationTemplateOutput.TemplateUrl), nil
+	return stringValue(res.TemplateUrl), nil
 }
 
 func addStackData(ctx context.Context, applicationList []Application)([]Application, error) {
 	if cloudformationClient == nil {
-		cloudformationClient = cloudformation.New(cfg)
+		cloudformationClient = getCloudformationClient()
 	}
-	req := cloudformationClient.ListStacksRequest(&cloudformation.ListStacksInput{
-		StackStatusFilter: []cloudformation.StackStatus{
-			cloudformation.StackStatusCreateComplete,
-			cloudformation.StackStatusCreateInProgress,
-			cloudformation.StackStatusDeleteInProgress,
+	res, err := cloudformationClient.ListStacks(ctx, &cloudformation.ListStacksInput{
+		StackStatusFilter: []types.StackStatus{
+			types.StackStatusCreate_complete,
+			types.StackStatusCreate_in_progress,
+			types.StackStatusDelete_in_progress,
 		},
 	})
-	res, err := req.Send(ctx)
 	if err != nil {
 		return nil, err
 	}
-	for _, i := range res.ListStacksOutput.StackSummaries {
-		stackName := aws.StringValue(i.StackName)
+	for _, i := range res.StackSummaries {
+		stackName := stringValue(i.StackName)
 		for n, j := range applicationList {
 			if strings.HasPrefix(stackName, j.Name) {
 				var url string
-				if i.StackStatus == cloudformation.StackStatusCreateComplete {
-					req_ := cloudformationClient.ListStackResourcesRequest(&cloudformation.ListStackResourcesInput{StackName: i.StackName})
-					res_, err := req_.Send(ctx)
+				if i.StackStatus == types.StackStatusCreate_complete{
+					res_, err := cloudformationClient.ListStackResources(ctx, &cloudformation.ListStackResourcesInput{StackName: i.StackName})
 					if err != nil {
 						log.Println(err)
 						break
 					}
-					for _, j := range res_.ListStackResourcesOutput.StackResourceSummaries {
-						if aws.StringValue(j.ResourceType) == "AWS::ApiGatewayV2::Api" {
-							url = "https://" + aws.StringValue(j.PhysicalResourceId) + ".execute-api." + os.Getenv("REGION") + ".amazonaws.com/"
+					for _, j := range res_.StackResourceSummaries {
+						if stringValue(j.ResourceType) == "AWS::ApiGatewayV2::Api" {
+							url = "https://" + stringValue(j.PhysicalResourceId) + ".execute-api." + os.Getenv("REGION") + ".amazonaws.com/"
 						}
 					}
 				}
@@ -203,17 +201,16 @@ func createStack(ctx context.Context, name string) error {
 	t := time.Now()
 	stackName := name + strings.Replace(t.Format(layout), ".", "", 1)
 	if cloudformationClient == nil {
-		cloudformationClient = cloudformation.New(cfg)
+		cloudformationClient = getCloudformationClient()
 	}
-	req := cloudformationClient.CreateStackRequest(&cloudformation.CreateStackInput{
-		Capabilities: []cloudformation.Capability{
-			cloudformation.CapabilityCapabilityIam,
-			cloudformation.CapabilityCapabilityAutoExpand,
+	_, err = cloudformationClient.CreateStack(ctx, &cloudformation.CreateStackInput{
+		Capabilities: []types.Capability{
+			types.CapabilityCapability_iam,
+			types.CapabilityCapability_auto_expand,
 		},
 		StackName: aws.String(stackName),
 		TemplateURL: aws.String(templateUrl),
 	})
-	_, err = req.Send(ctx)
 	if err != nil {
 		log.Println(err)
 		return err
@@ -223,12 +220,11 @@ func createStack(ctx context.Context, name string) error {
 
 func deleteStack(ctx context.Context, name string) error {
 	if cloudformationClient == nil {
-		cloudformationClient = cloudformation.New(cfg)
+		cloudformationClient = getCloudformationClient()
 	}
-	req := cloudformationClient.DeleteStackRequest(&cloudformation.DeleteStackInput{
+	_, err := cloudformationClient.DeleteStack(ctx, &cloudformation.DeleteStackInput{
 		StackName: aws.String(name),
 	})
-	_, err := req.Send(ctx)
 	if err != nil {
 		log.Println(err)
 	}
@@ -246,12 +242,95 @@ func getTargetStack(name string, list []Stack) Stack {
 	return stack
 }
 
-func init() {
+func getSARClient() *serverlessapplicationrepository.Client {
+	if cfg.Region != os.Getenv("REGION") {
+		cfg = getConfig()
+	}
+	return serverlessapplicationrepository.NewFromConfig(cfg)
+}
+
+func getCloudformationClient() *cloudformation.Client {
+	if cfg.Region != os.Getenv("REGION") {
+		cfg = getConfig()
+	}
+	return cloudformation.NewFromConfig(cfg)
+}
+
+func getConfig() aws.Config {
 	var err error
-	cfg, err = external.LoadDefaultAWSConfig()
-	cfg.Region = os.Getenv("REGION")
+	newConfig, err := config.LoadDefaultConfig()
+	newConfig.Region = os.Getenv("REGION")
 	if err != nil {
 		log.Print(err)
+	}
+	return newConfig
+}
+
+func stringValue(i interface{}) string {
+	var buf bytes.Buffer
+	strVal(reflect.ValueOf(i), 0, &buf)
+	res := buf.String()
+	return res[1:len(res) - 1]
+}
+
+func strVal(v reflect.Value, indent int, buf *bytes.Buffer) {
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	switch v.Kind() {
+	case reflect.Struct:
+		buf.WriteString("{\n")
+		for i := 0; i < v.Type().NumField(); i++ {
+			ft := v.Type().Field(i)
+			fv := v.Field(i)
+			if ft.Name[0:1] == strings.ToLower(ft.Name[0:1]) {
+				continue // ignore unexported fields
+			}
+			if (fv.Kind() == reflect.Ptr || fv.Kind() == reflect.Slice) && fv.IsNil() {
+				continue // ignore unset fields
+			}
+			buf.WriteString(strings.Repeat(" ", indent+2))
+			buf.WriteString(ft.Name + ": ")
+			if tag := ft.Tag.Get("sensitive"); tag == "true" {
+				buf.WriteString("<sensitive>")
+			} else {
+				strVal(fv, indent+2, buf)
+			}
+			buf.WriteString(",\n")
+		}
+		buf.WriteString("\n" + strings.Repeat(" ", indent) + "}")
+	case reflect.Slice:
+		nl, id, id2 := "", "", ""
+		if v.Len() > 3 {
+			nl, id, id2 = "\n", strings.Repeat(" ", indent), strings.Repeat(" ", indent+2)
+		}
+		buf.WriteString("[" + nl)
+		for i := 0; i < v.Len(); i++ {
+			buf.WriteString(id2)
+			strVal(v.Index(i), indent+2, buf)
+			if i < v.Len()-1 {
+				buf.WriteString("," + nl)
+			}
+		}
+		buf.WriteString(nl + id + "]")
+	case reflect.Map:
+		buf.WriteString("{\n")
+		for i, k := range v.MapKeys() {
+			buf.WriteString(strings.Repeat(" ", indent+2))
+			buf.WriteString(k.String() + ": ")
+			strVal(v.MapIndex(k), indent+2, buf)
+			if i < v.Len()-1 {
+				buf.WriteString(",\n")
+			}
+		}
+		buf.WriteString("\n" + strings.Repeat(" ", indent) + "}")
+	default:
+		format := "%v"
+		switch v.Interface().(type) {
+		case string:
+			format = "%q"
+		}
+		fmt.Fprintf(buf, format, v.Interface())
 	}
 }
 
